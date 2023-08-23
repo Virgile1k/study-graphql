938184b64c12e889542ce0ed8bc1a5a46616a77d
Vg1k <ndayambajevg16bussiness@gmail.com>
2023-08-23 10:20:40 +0200

    check dep

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,6 +1,6 @@
 const {ApolloServer } = require("apollo-server");
-const {typeDefs} =require ("./schema/types-defs");
-const {resolvers} =require("./schema/resolvers");
+const {typeDefs} =require ("../../schema/types-defs");
+const {resolvers} =require("../../schema/resolvers");
 
 const server =new ApolloServer({typeDefs,resolvers });
 server.listen().then(({url}) => {
