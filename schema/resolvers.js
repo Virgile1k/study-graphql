4f0dc22f034defb198f29908e45e0295c0af54d5
Vg1k <ndayambajevg16bussiness@gmail.com>
2023-08-23 11:27:42 +0200

    collabor

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,10 +1,35 @@
-const { UserList } = require("../fakedata");
+const { UserList, MovieList } = require("../fakedata");
+const _ = require("lodash");
 
 const resolvers = {
   Query: {
-    users() {
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    }, 
+    // Movie resolvers
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
     },
   },
 };
+
 module.exports = { resolvers };
